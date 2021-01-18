1eb6875389c2f812e69d9293e68394c1c380f801
JohnCorby <williamforgames6@gmail.com>
2021-01-18 01:35:17 -0800

    copying example even more

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use pest::iterators::Pair;
 use pest::Parser;
 use pest_derive::Parser;
 
@@ -6,5 +7,20 @@ use pest_derive::Parser;
 pub struct MyParser;
 
 fn main() {
-    println!("{:#?}", MyParser::parse(Rule::file, "-273.15,3215\r\n"));
+    let file: Pair<Rule> = MyParser::parse(Rule::file, "-273.15,3215\r\n")
+        .unwrap()
+        .next()
+        .unwrap();
+    for record in file.into_inner() {
+        match record.as_rule() {
+            Rule::record => {
+                let mut inner = record.into_inner();
+                let left: f32 = inner.next().unwrap().as_str().parse().unwrap();
+                let right: f32 = inner.next().unwrap().as_str().parse().unwrap();
+                println!("got record {}, {}", left, right);
+            }
+            Rule::EOI => (),
+            _ => unreachable!(),
+        }
+    }
 }
