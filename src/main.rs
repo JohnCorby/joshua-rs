07a0fdbacf850d453875573166d0f7f975e80008
JohnCorby <williamforgames6@gmail.com>
2021-01-18 01:23:22 -0800

    lets go

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct MyParser;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:#?}", MyParser::parse(Rule::file, "-273.15,3215\r\n"));
 }
