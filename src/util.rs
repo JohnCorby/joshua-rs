4e6fd39138a33433c7c3cc6c07eef58948743cc5
JohnCorby <williamforgames6@gmail.com>
2021-01-25 15:02:53 -0800

    dont use extend macro cuz it doesnt resolve :(

--- src/util.rs
+++ src/util.rs
@@ -2,22 +2,29 @@ use crate::error::{MyResult, Pos};
 use crate::parse::{Pair, Pairs, Rule};
 use crate::visit::Visit;
 use console::style;
-use extend::ext;
 
-#[ext(pub, name = PairExt)]
-impl<'i> Pair<'i> {
+pub trait PairExt<'i> {
     /// turns visit into an extension method for pair
+    fn visit<T: Visit>(self) -> MyResult<T>;
+
+    /// update pos (for errors)
+    fn track(self) -> Self;
+
+    /// check that a pair matches a rule, and then return its inner pairs
+    fn into_inner_checked(self, expected: Rule) -> MyResult<Pairs<'i>>;
+
+    fn to_pretty_string(&self) -> String;
+}
+impl<'i> PairExt<'i> for Pair<'i> {
     fn visit<T: Visit>(self) -> MyResult<T> {
         T::visit(self.track())
     }
 
-    /// update pos (for errors)
     fn track(self) -> Self {
         Pos::update(&self);
         self
     }
 
-    /// check that a pair matches a rule, and then return its inner pairs
     fn into_inner_checked(self, expected: Rule) -> MyResult<Pairs<'i>> {
         let actual = self.as_rule();
         if expected == actual {
@@ -41,10 +48,14 @@ impl<'i> Pair<'i> {
     }
 }
 
-#[ext(pub, name = PairsExt)]
-impl<'i> Pairs<'i> {
+pub trait PairsExt {
     /// visits any not iterated pairs,
     /// short circuiting if any of them error
+    fn visit_rest<T: Visit>(self) -> MyResult<Vec<T>>;
+
+    fn to_pretty_strings(&self) -> Vec<String>;
+}
+impl PairsExt for Pairs<'_> {
     fn visit_rest<T: Visit>(self) -> MyResult<Vec<T>> {
         self.map(Pair::visit).collect()
     }
