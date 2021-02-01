4266cf5f408681be3516a17adc414152a112cb8b
JohnCorby <williamforgames6@gmail.com>
2021-02-01 12:39:45 -0800

    switch to windows

--- src/compile.rs
+++ src/compile.rs
@@ -8,12 +8,12 @@ pub fn compile_program(c_code: impl AsRef<str>, path: impl AsRef<Path>) {
     Pos::reset();
 
     let c_path = &path.as_ref().with_extension("c");
-    let out_path = &path.as_ref().with_extension("");
+    let out_path = &path.as_ref().with_extension("exe");
 
     std::fs::write(c_path, c_code.as_ref()).unwrap();
 
     println!("compiling");
-    let status = Command::new("gcc")
+    let status = Command::new("clang")
         .arg(c_path)
         .arg("-o")
         .arg(out_path)
