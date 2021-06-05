d17f02967fbf172ec69a50886741bf23f7a07d22
JohnCorby <williamforgames6@gmail.com>
2021-06-04 22:28:58 -0700

    make generic functions different from normal so you cant call a normal func from a generic func call

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -6,26 +6,43 @@ pub mod late_init;
 
 pub trait Mangle {
     fn mangle(&self) -> String;
-    fn mangle_func(&self, arg_types: &[&Type<'_>]) -> String;
+    fn mangle_func(&self, arg_types: &[&Type<'_>], generic_replacements: &[&Type<'_>]) -> String;
 }
 impl Mangle for str {
     fn mangle(&self) -> String {
         format!("{}/*{}*/", mangling::mangle(self.as_bytes()), self)
     }
-    fn mangle_func(&self, arg_types: &[&Type<'_>]) -> String {
+    fn mangle_func(&self, arg_types: &[&Type<'_>], generic_replacements: &[&Type<'_>]) -> String {
         // don't mangle func main (entry point)
         if self == "main" {
             self.to_string()
         } else {
-            format!(
-                "{}({})",
-                self,
-                arg_types
-                    .iter()
-                    .map(|ty| ty.func_name())
-                    .collect::<Vec<_>>()
-                    .join(", ")
-            )
+            if generic_replacements.is_empty() {
+                format!(
+                    "{}({})",
+                    self,
+                    arg_types
+                        .iter()
+                        .map(|ty| ty.func_name())
+                        .collect::<Vec<_>>()
+                        .join(", ")
+                )
+            } else {
+                format!(
+                    "{}<{}>({})",
+                    self,
+                    generic_replacements
+                        .iter()
+                        .map(|it| it.func_name())
+                        .collect::<Vec<_>>()
+                        .join(", "),
+                    arg_types
+                        .iter()
+                        .map(|it| it.func_name())
+                        .collect::<Vec<_>>()
+                        .join(", ")
+                )
+            }
             .mangle()
         }
     }
